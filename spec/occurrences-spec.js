dd9085d100c123d801a26651361b901156c9d1e3
Chris Oloff <christian.oloff@gmail.com>
2014-03-07 17:21:15 +0200

    refactor files, wip

--- spec/occurrences-spec.js
+++ spec/occurrences-spec.js
@@ -3,6 +3,15 @@ var t = require('../tempex');
 var longAgo = new Date(1999, 0, 1, 6, 23, 15),
   farInTheFuture = new Date(2023, 4, 7, 11);
 
+describe("empty input", function() {
+  it("returns an empty list of occurrences", function() {
+    var occurrences = t.occurrences(/* temporal expressions */ [], /* from */ new Date(), /* to */ new Date());
+    expect(occurrences).toEqual([]);
+    occurrences = t.occurrences(/* temporal expressions */ [])
+    expect(occurrences).toEqual([]);
+  });
+});
+
 describe("single event occurring once", function() {
   it("occurrs once", function() {
     var event = { title: "my test event" },
