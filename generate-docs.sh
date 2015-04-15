6f2605e3c0cb1dee06f40477c97bdcc0b3356cb0
Chris Oloff <christian.oloff@gmail.com>
2015-04-15 17:24:02 +0200

    added comments on doc generation

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,3 +3,11 @@
 # Install jsdocs for this: http://usejsdoc.org/about-getting-started.html
 jsdoc tempex.js -d ./docs
 echo "Done, documentation is in ./docs"
+
+# after that:
+# git checkout gh-pages
+# git rebase master
+# TODO: maybe run 'jsdoc now'?
+# git add docs
+# git commit ...
+# git subtree push --prefix docs origin gh-pages
